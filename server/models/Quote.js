2a88c7681bf1c8d1def1ded11c00a9ae551d4048
Tyler Mifflin <you@example.com>
2023-07-31 21:06:01 -0600

    added a quote model, also included quote model in user model

---
+++ server/models/Quote.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const quoteSchema = new Schema({
+  quoteText: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const Quote = model('Quote', quoteSchema);
+
+module.exports = Thought;
