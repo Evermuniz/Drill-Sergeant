fae97426ced49e22178bfe53a6e60f65e7ba881d
Tyler Mifflin <you@example.com>
2023-07-31 21:14:49 -0600

    set up Goal model with comments and included it in the User model

--- server/models/User.js
+++ server/models/User.js
@@ -25,6 +25,12 @@ const userSchema = new Schema({
       ref: 'Quote',
     },
   ],
+  goal: [
+    { 
+      type: Schema.Types.ObjectId,
+      ref: 'Goal',
+    },
+  ],
 });
 
 userSchema.pre('save', async function (next) {
