c8ea383aa4c1d3131cd5d524aa8503f9875e4953
Tyler Mifflin <you@example.com>
2023-08-01 19:22:33 -0600

    added the exercise model

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -2,4 +2,20 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 exerciseSchema = new Schema({
-    
+    name: {
+        type: String,
+    },
+    type: {
+        type: String,
+    },
+    muscle: {
+        type: String,
+    },
+    difficulty: {
+        type: String,
+    },
+});
+
+const Exercise = model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
