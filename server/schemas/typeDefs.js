38cf9ebfe28dc5d43c9a0401b326961f45621ebc
Tyler Mifflin <you@example.com>
2023-08-07 15:29:18 -0600

    updated User model for goals and workout and also typeDefs to match the changes for workout model, also made a const to define gql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,13 @@
+const { gql } = require('apollo-server-express');
+
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
     password: String
-    goals: [Goal]
-    Exercise: [Exercise]
+    goals: [Goal]!
+    workouts: [Workout]!
   }
 
   type Goal {
