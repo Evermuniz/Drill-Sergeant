de14a90751f71303d2925661e4a6a664b037281c
Tyler Mifflin <you@example.com>
2023-08-08 20:13:44 -0600

    still getting errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Goal, Quote, Workout, SetSchema, exerciseSchema } = require('../models');
+const { User, Goal, Quote, Workout, SetSchema, exerciseSchema, Exercise } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
