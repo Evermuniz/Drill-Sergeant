12f551a53c43fdb5e23da6fc8a9ea566b4df2f6c
Evermuniz <evermuniz@outlook.com>
2023-08-10 22:07:16 -0600

    mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route 
-                path="/home"
+                path="/"
                 element={<Home />}
               />
               <Route 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-    })
-    .catch(error => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-    });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Drill-Sergeant");
+
+
+
 
 
 module.exports = mongoose.connection;
