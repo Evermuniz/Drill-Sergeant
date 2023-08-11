a2037a709e6805f1fc4d014f2c914a231104502d
Evermuniz <evermuniz@outlook.com>
2023-08-10 21:15:30 -0600

    try 100

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB Atlas");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB Atlas:", error);
-  });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB Atlas:', error);
+    });
+
 
 module.exports = mongoose.connection;
