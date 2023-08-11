c6b8385f18f99e29e0eee3f163cfa3d2771fb175
Evermuniz <evermuniz@outlook.com>
2023-08-10 20:09:28 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DrillSergeant');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Drill-Sergeant");
 
 module.exports = mongoose.connection;
